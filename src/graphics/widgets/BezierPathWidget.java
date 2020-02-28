9b6996218d32f2c2de9c9347027a496750c2a167
NeilRajah <neil.balaskandarajah@gmail.com>
2020-02-28 16:17:59 -0500

    Add more configurability to JComponentFactory

---
+++ src/graphics/widgets/BezierPathWidget.java
@@ -0,0 +1,27 @@
+/**
+ * BezierPathWidget
+ * Author: Neil Balaskandarajah
+ * Created on: 02/22/2020
+ * Widget for interacting with Bezier Paths
+ */
+
+package graphics.widgets;
+
+import java.awt.GridBagLayout;
+
+import javax.swing.JPanel;
+
+public class BezierPathWidget extends JPanel {
+
+	public BezierPathWidget() {
+		this.setLayout(new GridBagLayout());
+		layoutView();
+	}
+
+	private void layoutView() {
+		for (int i = 0; i < 6; i++) {
+			
+			
+		}
+	}
+}
